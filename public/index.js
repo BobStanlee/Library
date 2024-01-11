ed4bd46932fc7a3178ea13c535bfbd5f890a74da
Bob Stanley <dakotasfacts@gmail.com>
2024-01-11 12:17:52 +0000

    -Done with addBookToLibrary functionality -Added isDuplicateBook functionality to check if newBook already exist

--- public/index.js
+++ public/index.js
@@ -12,9 +12,28 @@ function Book(title, author, page, status) {
   this.title = title,
   this.author = author,
   this.page = page,
-  this.status = status,
+  this.status = status
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title='test', author='test', page=300, status='test') {
   // do stuff here
+  let newBook = new Book(title, author, page, status);
+
+  // add book to library
+  console.log(myLibrary); // log before library was updated
+
+  if (!isDuplicateBook(newBook)) {
+    myLibrary.push(newBook);
+    console.log('Book added successfully!');
+    } else {
+    console.log('This book already exists in the library.');
+    }
+}
+
+
+// Function to check if Book already exist in the library
+function isDuplicateBook(newBook) {
+    return myLibrary.find(book => (
+        book.title === newBook.title && book.author === newBook.author
+    ));
 }
