8bbdc75d3fce020d3dffaf72360d65e1f68740e9
Bob Stanley <dakotasfacts@gmail.com>
2024-01-11 11:40:35 +0000

    Done with the initial Code structure in index.js

---
+++ public/index.js
@@ -0,0 +1,20 @@
+const myLibrary = [
+    {
+        title: "Bob's Life", 
+        author: 'Bob Stanley', 
+        page: 255,
+        status: 'read',
+    }
+];
+
+function Book(title, author, page, status) {
+  // the constructor...
+  this.title = title,
+  this.author = author,
+  this.page = page,
+  this.status = status,
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
