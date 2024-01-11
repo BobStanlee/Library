f80d65ca38521c570441b50e946a2ca329200816
Bob Stanley <dakotasfacts@gmail.com>
2024-01-11 12:51:02 +0000

    Done with the styling of the header -Added poppins from google fonts in input.css file -update fontfamily variables in tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'body': ['Poppins', 'Arial', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
